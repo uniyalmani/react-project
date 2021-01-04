8da03f66c27805d17497669f5a0aa1cf9e109945
uniyalmani <Ashutoshuniyal21@gmail.com>
2021-01-04 20:14:48 +0530

    added store

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import configureStore from './store'
+
+const store = configureStore();
+console.log(store.getState());
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+// import { combineReducers, createStore } from "redux";
+// import thunk from "redux-thunk";
+
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from "../reducers";
+import logger from "redux-logger";
+
+let store;
+
+export default function configureStore(){
+    store   = createStore(reducer, applyMiddleware(thunk, logger));
+    return store;
+}
